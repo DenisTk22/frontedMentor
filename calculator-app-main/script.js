c746a6bde0d746be13b7297d4c49b8ad78364e43
Denis Tkachev <allpost-s@ya.ru>
2024-02-07 19:10:14 +0300

    calc

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -1,11 +1,27 @@
 const calcButtonsEl = document.querySelector('.calc__buttons');
 const calcScreenEl = document.querySelector('.calc__screen');
 
+let d1 = '';
+let d2 = '';
+let temp = '';
+let result = 0;
 calcButtonsEl.addEventListener('click', e => {
-    if (e.target.className === 'calc__buttons_digit') {
-        let d1 = parseInt(e.target.textContent);
+    if (e.target.classList.contains('calc__buttons_digit') || e.target.classList.contains('calc__buttons_dot')) {
+        temp = e.target.textContent;
+        d1 = d1 + temp;
         calcScreenEl.textContent = d1;
+        // d1 = parseFloat(d1);
+    } else if (e.target.classList.contains('calc__buttons_button-reset')) {
+        calcScreenEl.textContent = '';
+        d1 = '';
+    // } else if (e.target.classList.contains('calc__buttons_plus')) {
+    //     temp = '';
+    //     if (e.target.classList.contains('calc__buttons_digit') || e.target.     classList.contains('calc__buttons_dot')) {
+    //         temp = e.target.textContent;
+    //         d2 = d2 + temp;
+    //         calcScreenEl.textContent = d2;
+           
+    //     } 
         
-    }
-    // calcScreenEl.textContent = e.target.textContent;
-})
+    // }
+});
