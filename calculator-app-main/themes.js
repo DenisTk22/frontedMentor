abfbf2b736f0408313568217b77ef12a724a2600
Denis Tkachev <allpost-s@ya.ru>
2024-02-12 15:23:20 +0300

    calc

--- calculator-app-main/themes.js
+++ calculator-app-main/themes.js
@@ -7,25 +7,14 @@ const themeClassicEl = document.querySelector('.theme-form_theme-classic');
 const themeDarkEl = document.querySelector('.theme-form_theme-dark');
 const themeLightEl = document.querySelector('.theme-form_theme-light');
 
-// if (themeClassicEl.hasAttribute('checked')) {
-//     calcBodyEl.style.backgroundColor = 'green';
-// }
-
-// if (themeDarkEl.checked) {
-//     calcBodyEl.style.backgroundColor = 'darkblue';
-// }
-
 calcThemesEl.addEventListener('change', e => {
     if (e.target.className === 'theme-form_theme-classic') {
-        console.log('green');
-        calcBodyEl.style.backgroundColor = 'purple';
+        calcBodyEl.style.backgroundColor = 'gainsboro';
     } 
     if (e.target.className === 'theme-form_theme-dark') {
-        console.log('grdarkblue');
         calcBodyEl.style.backgroundColor = 'darkblue';
     }
     if (e.target.className === 'theme-form_theme-light') {
-        console.log('yellow');
         calcBodyEl.style.backgroundColor = 'yellow';
     }
-})
+});
